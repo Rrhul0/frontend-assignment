6f85f62e4a1c36e3d3cc96574ae8a88f34255462
Rrhul0 <rr025765@gmail.com>
2022-11-08 15:36:49 +0530

    added: fully working sign in with metamask wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.css'
+import { ethers } from 'ethers'
+import { SiweMessage } from 'siwe'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    async function onClickSignIn() {
+        //(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        const domain = window.location.host
+        const origin = window.location.origin
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+        await provider.send('eth_requestAccounts', []) //.catch(() => console.log('user rejected request'))
+
+        const signer = provider.getSigner()
+
+        function createSiweMessage(address: string, statement: string) {
+            const message = new SiweMessage({
+                domain,
+                address,
+                statement,
+                uri: origin,
+                version: '1',
+                chainId: 1,
+            })
+            return message.prepareMessage()
+        }
+        const message = createSiweMessage(await signer.getAddress(), 'Sign in with Ethereum to the app.')
+        console.log(await signer.signMessage(message))
+    }
+
+    return (
+        <div className='App'>
+            <button
+                onClick={e => {
+                    onClickSignIn().then(() => console.log('done'))
+                }}>
+                Sign in with Etherium (MetaMask Wallet)
+            </button>
+        </div>
+    )
 }
 
-export default App;
+export default App
