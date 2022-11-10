f0e3cf7e10b0a18bac45ef4f96f153651eeb6e3c
Rrhul0 <rr025765@gmail.com>
2022-11-10 23:59:55 +0530

    added layout component and share it to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import SignIn from './SignIn'
 import Dashboard from './Dashboard'
 import Stats from './Stats'
 import Page404 from './404'
+import Layout from './layout'
 export default function App() {
     return (
         <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='stats' element={<Stats />} />
-            <Route path='signin' element={<SignIn />} />
-            <Route path='*' element={<Page404 />} />
+            <Route path='/' element={<Layout />}>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='stats' element={<Stats />} />
+                <Route path='signin' element={<SignIn />} />
+                <Route path='*' element={<Page404 />} />
+            </Route>
         </Routes>
     )
 }
