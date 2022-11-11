085540888d99f2b4cca1e44c69e05e25f185f775
Rrhul0 <rr025765@gmail.com>
2022-11-11 11:48:38 +0530

    added error 404 page and added some colors in tailwinds config

--- src/404.tsx
+++ src/404.tsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom'
+
 export default function Page404() {
-    return <></>
+    return (
+        <main className='flex flex-col items-start justify-center h-full w-fit mx-auto'>
+            <div className='rounded-2xl text-white bg-secondry py-1 px-2'>Page not found</div>
+            <h1 className='text-6xl font-bold text-primary py-5'>Oh No! Error 404</h1>
+            <h2 className='text-secondry font-semibold'>
+                You've moved so fast and got lost.
+                <br /> Come back to Homepage
+            </h2>
+            <Link to='/' className='bg-primaryBg text-[#E1E6F0] px-2.5 py-1.5 rounded-lg font-semibold my-4'>
+                Back to Homepage
+            </Link>
+        </main>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: { primary: '#1B325F', secondry: '#AABADA', primaryBg: '#4E79D7' },
+        },
     },
     plugins: [],
 }
