a13fdf3ffb8c11947ecb923de9565885824ddb77
Rrhul0 <rr025765@gmail.com>
2022-11-12 10:56:54 +0530

    remove unnecessary stats blocks in mobile screens

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -16,10 +16,10 @@ export default function Stats() {
     return (
         <div className='grid lg:grid-cols-6 lg:grid-rows-2 w-full h-full gap-8 p-4 '>
             <TxnsWidget address={address} />
-            <div className='bg-[#0B45F5] lg:col-span-2 shadow-zinc-700  shadow-2xl rounded-2xl p-8 flex items-center justify-center text-[#f59191] text-3xl font-bold'>
+            <div className='bg-[#0B45F5] lg:col-span-2 shadow-zinc-700  shadow-2xl rounded-2xl p-8 hidden lg:flex items-center justify-center text-[#f59191] text-3xl font-bold'>
                 Widget not configured
             </div>
-            <div className='bg-[#0B45F5] lg:col-span-3  shadow-zinc-700  shadow-2xl rounded-2xl p-8 flex items-center justify-center text-[#f59191] text-3xl font-bold'>
+            <div className='bg-[#0B45F5] lg:col-span-3  shadow-zinc-700  shadow-2xl rounded-2xl p-8 hidden lg:flex items-center justify-center text-[#f59191] text-3xl font-bold'>
                 Widget not configured
             </div>
             <BlockHeightWidget />
